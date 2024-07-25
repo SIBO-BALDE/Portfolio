7ca56b6fe908a3ce96e36aaea956b91ebf8f59e2
SIBO-BALDE <baldesibo@gmail.com>
2024-07-25 13:46:47 +0000

    changement

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const typewriter = new Typewriter(textAnime, {
     loop: true, // Si vous souhaitez que l'animation se répète
 });
 
-typewriter.typeString("Frontend web developer")
+typewriter.typeString("React js web developer ")
     .pauseFor(300)
     .deleteAll() // Supprime le texte actuel
     // .typeString("Nouveau texte à animer")
